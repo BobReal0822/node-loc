7e9b6dc2424c3bd8ca30e9ebbb6aa8ccd72f61a0
ephoton <yangyang.mayangyan@alibaba-inc.com>
2017-04-01 16:12:52 +0800

    feat: add function: getFileInfo

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,6 @@
+import { Deflate } from 'zlib';
+import { encode } from 'punycode';
+import * as buffer from 'buffer';
 /**
  * 
  */
@@ -5,7 +8,8 @@
 import * as Fs from 'fs';
 import * as Path from 'path';
 
-let languageMap = require('language-map');
+const languageMap = require('language-map');
+const lang = require('language-classifier');
 
 // const VerderPathConfig = {
 //   extensions: Path.join(__dirname, '../vender', 'extensions.json')
@@ -13,43 +17,150 @@ let languageMap = require('language-map');
 
 // Fs.writeFileSync(VerderPathConfig.extensions, JSON.stringify(extensions, null, 2));
 
+interface ExtensionsTypes {
+  [key: string]: string;
+};
+
+export interface FileOptions {
+  sync?: boolean;
+}
+
+export interface DetectorOptions {
+
+}
+
+export interface DetectedFileInfo {
+  fileType: string;
+  fileSize: number;
+  totalLine: number;
+  lines: {
+    [key: string]: number
+  }
+}
+
+const DefaultDetectorOptions: DetectorOptions = {
+
+};
+
+const DefaultDatectedFileInfo: DetectedFileInfo = {
+  fileType: '',
+  fileSize: 0,
+  totalLine: 0,
+  lines: {}
+};
+
+/**
+ * language detector
+ * 
+ * @export
+ * @class LanguageDetector
+ */
 export class LanguageDetector {
   private static extensionMap: {
     [key: string]: string;
   } = {};
 
-  constructor() {
+  private options: DetectorOptions;
+
+  constructor(options?: DetectorOptions) {
     return new LanguageDetector();
   };
 
-  public static detect(filePath: string): string {
+  /**
+   * get language extension map from 'language-map'
+   * 
+   * @private
+   * @static
+   * @returns {extensionsTypes} 
+   * 
+   * @memberOf LanguageDetector
+   */
+  private static getLanguageExtensionMap(): ExtensionsTypes {
+    let extensions: ExtensionsTypes = {};
+
+    Object.keys(languageMap).map(language => {
+      let languageMode = languageMap[language],
+        languageExtensions = languageMode && languageMode.extensions || [];
+
+      languageExtensions.map((extension: string) => {
+        extensions[extension.toLowerCase()] = language.toLowerCase();
+      });
+    });
+
+    return extensions;
+  }
+
+  /**
+   * get language type by filePath
+   * 
+   * @static
+   * @param {string} filePath 
+   * @returns {string} 
+   * 
+   * @memberOf LanguageDetector
+   */
+  public static getFileType(filePath: string): string {
     let fileExtension = '.' + filePath.split('.').pop();
 
     if (!Object.keys(this.extensionMap).length) {
-      this.extensionMap = getLanguageExtensionMap();
+      this.extensionMap = this.getLanguageExtensionMap();
     }
 
     // console.log("in detect: \n", fileExtension, '\n');
     return fileExtension ? (this.extensionMap[fileExtension] || '') : '';
   }
-};
 
+  /**
+   * get language type by code snippet
+   * 
+   * @static
+   * @param {string} codeSnippet 
+   * @returns 
+   * 
+   * @memberOf LanguageDetector
+   */
+  public static classify(codeSnippet: string) {
+    return codeSnippet ? lang(codeSnippet) : '';
+  }
 
-function getLanguageExtensionMap(): {
-  [key: string]: string;
-} {
-  let extensions: {
-    [key: string]: string;
-  } = {};
+  /**
+   * get file info by file path
+   * 
+   * @static
+   * @param {string} filePath 
+   * @param {fileOptionsInfo} [options] 
+   * @returns 
+   * 
+   * @memberOf LanguageDetector
+   */
+  public static getFileInfo(filePath: string, options?: FileOptions): DetectedFileInfo {
+    let fileInfo: DetectedFileInfo = Object.assign({}, DefaultDatectedFileInfo);
+    let fileStat: Fs.Stats;
+    let fileData: string;
+    let lines: string[];
 
-  Object.keys(languageMap).map(language => {
-    let languageMode = languageMap[language],
-      languageExtensions = languageMode && languageMode.extensions || [];
+    try {
+      fileStat = Fs.statSync(filePath);
+      fileData = Fs.readFileSync(filePath, 'utf-8');
+    } catch (err) {
+      throw new Error('read file failed.');
+    }
 
-    languageExtensions.map((extension: string) => {
-      extensions[extension.toLowerCase()] = language.toLowerCase();
+    lines = fileData.split(/\n/);
+    fileInfo.fileSize = fileStat && fileStat.size || 0;
+    fileInfo.totalLine = lines && lines.length || 0;
+    fileInfo.fileType = this.getFileType(filePath);
+
+    lines.map((line, index) => {
+      let languageType: string = lang(line);
+
+      if (!fileInfo.lines[languageType]) {
+        fileInfo.lines[languageType] = 1;
+      } else {
+        fileInfo.lines[languageType] ++;
+      }
     });
-  });
 
-  return extensions;
+    return fileInfo;
+  }
 };
--- test/index.ts
+++ test/index.ts
@@ -5,7 +5,8 @@ import * as Path from 'path';
 import * as assert from 'assert';
 import { LanguageDetector } from './../src/';
 
-let languageMap = require('language-map');
+const languageMap = require('language-map');
+const lang = require('language-classifier');
 
 const JsPath = Path.join(__dirname, '../..', 'data/test1.cpp');
 
@@ -13,8 +14,22 @@ describe('Test node-language-detect', () => {
   describe('javascipt', () => {
     it('should return javascipt: ', () => {
         console.log('JsPath: ', JsPath);
-        console.log('-----: ', LanguageDetector.detect(JsPath));
+        console.log('-----: ', LanguageDetector.getFileType(JsPath));
+        console.log("___________get file info: ", LanguageDetector.getFileInfo(JsPath));
     });
   });
 });
 
+describe('Test node-language-classifier', () => {
+  describe('javascipt', () => {
+    it('should return javascipt: ', () => {
+        console.log('-----: ',lang('var a = 2;'));
+    });
+  });
+
+  describe('c++', () => {
+    it('should return c++: ', () => {
+        console.log('-----: ', lang('let a = [];'));
+    });
+  });
+});
